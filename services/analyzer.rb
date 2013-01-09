51d1f516289f6ff74784e4a14dce2b3e6c788066
Tony Kovanen <tonykovanen@hotmail.com>
2013-01-09 18:19:20 +0200

    More stuff to skeletons

--- services/analyzer.rb
+++ services/analyzer.rb
@@ -1,3 +1,27 @@
 class Analyzer
+  attr_reader :tree
 
+  def initialize(tree)
+    @tree = tree
+  end
+
+  def run_insertions
+    # TODO
+  end
+
+  def run_and_analyze_insertions
+    # TODO
+  end
+
+  def analyze_queries
+    # TODO
+  end
+
+  private
+  def time
+    start_time = Time.now
+    yield
+    end_time = Time.now
+    end_time - start_time
+  end
 end
--- utils/filehandler.rb
+++ utils/filehandler.rb
@@ -1,3 +1,37 @@
 class FileHandler
+  attr_reader :filename, :type
 
+  def initialize(filename, type)
+    @types = [:insertion, :query]
+    @handlers = [:create_insertion, :create_query]
+    @filename = filename
+    @type = type
+    @file = nil
+  end
+
+  def begin
+    @file = File.open(filename, "r")
+  end
+
+  def finished?
+    @file.any?
+  end
+
+  def handle_row
+    line = @file.readline
+    send(handlers[@type], line)
+  end
+
+  def close
+    @file.close
+  end 
+
+  private
+  def create_insertion(line)
+    # TODO, returns one single point to be inserted
+  end
+
+  def create_query(line)
+    # TODO, returns one single rectangle to be 
+  end
 end
