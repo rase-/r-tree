ee5da09abae0364cc34b9621e41c307152fdad4a
Tony Kovanen <tonykovanen@hotmail.com>
2013-01-09 21:40:10 +0200

    Skeleton for main program

---
+++ run_experiment.rb
@@ -0,0 +1,10 @@
+require "rubygems"
+require "geoutm"
+require_relative "model/quadtree.rb"
+require_relative "services/analyzer.rb"
+
+# The code to decide space and initialize trees
+
+# The code to initialize analyzer with appropriate data
+
+# The code to run analysis and print results
