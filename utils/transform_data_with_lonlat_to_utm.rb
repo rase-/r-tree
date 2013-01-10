046e01e201493d61469160d91e9ee24c8633b979
Tony Kovanen <tonykovanen@hotmail.com>
2013-01-10 14:54:51 +0200

    Fixed bug in conversion, only had half of the total maount previously converted

--- utils/transform_data_with_lonlat_to_utm.rb
+++ utils/transform_data_with_lonlat_to_utm.rb
@@ -5,16 +5,16 @@ require "geoutm"
 # Requires a dataset in csv form of columns lon,lat in a given input file (arg 1)
 converted = []
 file = File.open(ARGV[0], "r")
-while (file.any?)
-  line = file.readline
+lines = file.readlines
+file.close
+lines.each do |line|
   lon = line.split.first.to_f
   lat = line.split.last.to_f
   lonlat = GeoUtm::LatLon.new(lat, lon)
   utm = lonlat.to_utm
-  # All given data is assumed to be in the same zone, if not, we assume that serach queries only target locations in the same utm zone
+  # All given data is assumed to be in the same zone, if not, we assume that search queries only target locations in the same utm zone
   converted << "#{utm.e},#{utm.n},#{utm.zone}"
 end
-file.close
 
 # Prints out a csv file of form e,n,zone, a UTM equivalent of the given lon,lat coordinate pair to given output file (arg 2)
 file = File.new(ARGV[1], "w")
