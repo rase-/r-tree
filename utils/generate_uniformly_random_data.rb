3f008a45b3445f1f5de3eccc0c246a2cbc224706
Tony Kovanen <tonykovanen@hotmail.com>
2013-01-10 15:05:07 +0200

    Generated 100000 examples of random data, and refactored data generation very minorly

--- utils/generate_uniformly_random_data.rb
+++ utils/generate_uniformly_random_data.rb
@@ -3,9 +3,11 @@ x = 0
 y = 0
 width = 100000
 height = 100000
+outputfilename = ARGV[0]
+number_of_examples = ARGV[1].to_i
 
-file = File.new(ARGV[0], "w")
-(1..ARGV[1].to_i).each do |i|
+file = File.new(outputfilename, "w")
+(1..number_of_examples).each do |i|
   # format is x,y
   file.write("#{x + Random.rand(0..width)},#{y + Random.rand(0..height)}\n")
 end
