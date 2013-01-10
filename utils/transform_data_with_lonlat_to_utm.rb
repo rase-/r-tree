60414d342216b9f681e7c3ab7be47354d2900cf3
Tony Kovanen <tonykovanen@hotmail.com>
2013-01-10 11:56:09 +0200

    Data converted to UTM format, however not integerified, in case we want to move the point to the right or something. Let's handle this in the FileHandler

--- utils/transform_data_with_lonlat_to_utm.rb
+++ utils/transform_data_with_lonlat_to_utm.rb
@@ -1,16 +1,20 @@
 require "rubygems"
+gem "geoutm"
 require "geoutm"
 
 # Requires a dataset in csv form of columns lon,lat in a given input file (arg 1)
 converted = []
-File.open(ARGV[0], "r").do |line|
+file = File.open(ARGV[0], "r")
+while (file.any?)
+  line = file.readline
   lon = line.split.first.to_f
   lat = line.split.last.to_f
-  lonlat = GeoUtm::LonLat.new(lon, lat)
+  lonlat = GeoUtm::LatLon.new(lat, lon)
   utm = lonlat.to_utm
   # All given data is assumed to be in the same zone, if not, we assume that serach queries only target locations in the same utm zone
-  converted << "#{utm.e},#{utm.n},#{utn.zone}"
-end.close
+  converted << "#{utm.e},#{utm.n},#{utm.zone}"
+end
+file.close
 
 # Prints out a csv file of form e,n,zone, a UTM equivalent of the given lon,lat coordinate pair to given output file (arg 2)
 file = File.new(ARGV[1], "w")
