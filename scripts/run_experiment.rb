322a0997ebb1ec70101ba039e111c7ec47ccd7c9
Tony Kovanen <tonykovanen@hotmail.com>
2013-01-13 11:24:40 +0200

    Added possibility for further optional command line arguments to initialize trees with wanted parameters, making experimentation easier

--- scripts/run_experiment.rb
+++ scripts/run_experiment.rb
@@ -9,17 +9,30 @@ spaces = {
   generated: BoundingBox.new(Point.new(0,0), 100000, 100000)
 }
 
+tree_parameters = {
+  q_max_elem: 50,
+  q_max_depth: nil,
+  r_max: 50,
+  r_min: 15
+}
+
 # Initialize  
 datafilename = ARGV[0]
 queryfilename = ARGV[1]
 space_selected = ARGV[2].to_sym
+# Parse additional options to either trees parameters
+# Arguments provided with -- switch, which is dropped
+options = ARGV.drop 3 # All but first three arguments
+(0..(options.count - 1)).step(2) do |i|
+  tree_parameters[options[i][2..-1].to_sym] = options[i+1].to_i
+end
 
 # The code to decide space and initialize trees
 space = spaces[space_selected]
-quadtree = QuadTree.new space
-rtree = RTree.new space
+quadtree = QuadTree.new space, tree_parameters[:q_max_elem], tree_parameters[:q_max_depth]
+rtree = RTree.new space, tree_parameters[:r_max], tree_parameters[:r_min]
 
-# The code to initialize analyzer with appropriate data, maybe change data files to be sent as parameters
+# The code to initialize analyzer with appropriate data
 quadtree_analyzer = Analyzer.new quadtree, datafilename, queryfilename
 rtree_analyzer = Analyzer.new rtree, datafilename, queryfilename
 
